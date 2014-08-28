455234cef9bc190b4622c71bc8dceb3a57304035
Jake Wilson <jake.e.wilson@gmail.com>
2014-08-28 17:14:40 -0600

    Initial constructor development
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- src/Palette/Palette.php
+++ src/Palette/Palette.php
@@ -1,8 +1,29 @@
 <?php
 
-namespace Palette;
+// namespace Palette;
 
 class Palette
 {
+	const REGEX_HEX = "/^\#?[0-9A-F]{6}$/i";
+	const REGEX_HEX_SHORT = "/^\#?[0-9A-F]{3}$/i";
+	const REGEX_RGB_DEC = "/^rgb\([0-255]+,[0-255]+,[0-255]+\)$/i";
 
+	public function __construct($color = null)
+	{
+		// Hex
+		if ( preg_match(self::REGEX_HEX, $color) ) {
+			// Remove hash
+			$color = trim($color, '#');
+
+			$this->red
+		} else if ( preg_match(self::REGEX_HEX_SHORT, $color) ) { // Shorthand Hex
+			// Remove hash
+			$color = trim($color, '#');
+
+
+		} else if ( preg_match(self::REGEX_RGB_DEC, $color) ) { // RGB Decimal
+
+		}
+
+	}
 }
