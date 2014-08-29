fcf48b26a77b116a08cb2df2fa9d653ce61e20d4
Jake Wilson <jake.wilson@gunsightlabs.com>
2014-08-29 00:16:54 -0600

    Format incoming color. Remove whitespace. Set to lowercase. Improved comment formatting.

--- src/Palette/Palette.php
+++ src/Palette/Palette.php
@@ -4,24 +4,38 @@
 
 class Palette
 {
-	const REGEX_HEX = "/^\#?[0-9A-F]{6}$/i";
-	const REGEX_HEX_SHORT = "/^\#?[0-9A-F]{3}$/i";
-	const REGEX_RGB_DEC = "/^rgb\([0-255]+,[0-255]+,[0-255]+\)$/i";
+	const REGEX_HEX = "/^\#?[0-9A-F]{6}$/";
+	const REGEX_HEX_SHORT = "/^\#?[0-9A-F]{3}$/";
+	const REGEX_RGB_DEC = "/^rgb\([0-255]+,[0-255]+,[0-255]+\)$/";
 
 	public function __construct($color = null)
 	{
-		// Hex
+		// Normalize string (lowercase, remove whitespace)
+		$color = preg_replace("/\s+/", "", strtolower($color));
+		var_dump($color);
+
+		// Determine color format
 		if ( preg_match(self::REGEX_HEX, $color) ) {
+			// Hex
+
 			// Remove hash
 			$color = trim($color, '#');
 
-			$this->red
-		} else if ( preg_match(self::REGEX_HEX_SHORT, $color) ) { // Shorthand Hex
+			var_dump("hex");
+
+
+		} else if ( preg_match(self::REGEX_HEX_SHORT, $color) ) {
+			// Shorthand Hex
+
 			// Remove hash
 			$color = trim($color, '#');
 
+			var_dump("hex short");
 
-		} else if ( preg_match(self::REGEX_RGB_DEC, $color) ) { // RGB Decimal
+		} else if ( preg_match(self::REGEX_RGB_DEC, $color) ) {
+			// RGB Decimal
+
+			var_dump("rgb dec");
 
 		}
 
