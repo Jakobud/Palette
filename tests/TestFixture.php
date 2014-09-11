6d6604d9635cfd01e80ddcf078c037de9c2f71a4
Jake Wilson <jake.e.wilson@gmail.com>
2014-09-11 13:52:41 -0600

    Changed Test Fixture
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- tests/TestFixture.php
+++ tests/TestFixture.php
@@ -2,12 +2,8 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-use Palette\Palette;
-
-class TestFixture extends \PHPUnit_Framework_TestCase
+class TestFixture extends PHPUnit_Framework_TestCase
 {
-	protected function setUp()
-	{
-
-	}
+	protected function setUp(){}
+	protected function tearDown(){}
 }
